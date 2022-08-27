828af164288119a0c497dd64efe84126f0eefc4d
Don Isaac <donald.isaac@gmail.com>
2022-08-27 17:52:35 -0400

    feat(utils): add assertion function

--- src/util.ts
+++ src/util.ts
@@ -1,28 +1,34 @@
-import { extent } from 'd3-array';
+// import { extent } from 'd3-array';
 
-// eslint-disable-next-line import/prefer-default-export
-export function encodeFloatsRGBArange(values, array) {
-  // Rescale a number into the range [0, 1e32]
-  // so that it can be passed to the four components of a texture.
-  values = values.flat();
-
-  const [min, max] = extent(values);
-
-  if (array === undefined) {
-    array = new Uint32Array(values.length);
+export function assert(cond: unknown, message?: string): asserts cond {
+  if (!cond) {
+    throw new Error(message ?? 'Assertion failed');
   }
-
-  const scale_size = (2 ** 32) / (max - min);
-
-  let i = 0;
-
-  for (const value of values) {
-    array[i] = (value - min) * scale_size;
-    i += 1;
-  }
-
-  return {
-    extent: [min, max],
-    array: new Uint8Array(array.buffer),
-  };
 }
+// TODO: This function is unused and appears broken. Commenting out for now.
+// eslint-disable-next-line import/prefer-default-export
+// export function encodeFloatsRGBArange(values, array) {
+//   // Rescale a number into the range [0, 1e32]
+//   // so that it can be passed to the four components of a texture.
+//   values = values.flat();
+
+//   const [min, max] = extent(values);
+
+//   if (array === undefined) {
+//     array = new Uint32Array(values.length);
+//   }
+
+//   const scale_size = (2 ** 32) / (max - min);
+
+//   let i = 0;
+
+//   for (const value of values) {
+//     array[i] = (value - min) * scale_size;
+//     i += 1;
+//   }
+
+//   return {
+//     extent: [min, max],
+//     array: new Uint8Array(array.buffer),
+//   };
+// }
