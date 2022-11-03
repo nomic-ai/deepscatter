1e0f3d76bf028818fabc73f27ff7e0792789be1a
David Mora <davidnmora@gmail.com>
2022-11-02 23:32:49 -0700

    Revert "Add a "npm run dev-simple" command & documentation for the new sample html file"
    This reverts commit 3ec6989b7d580160b17aedbfc2ae60201c5b5b21.

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,6 +1,5 @@
-const glslify = require("rollup-plugin-glslify");
-import { defineConfig } from "vite";
 
+const glslify = require('rollup-plugin-glslify');
 //import worker from 'rollup-plugin-web-worker-loader';
 
 //import { fileURLToPath } from 'url';
@@ -8,28 +7,24 @@ import { defineConfig } from "vite";
 //const __filename = fileURLToPath(import.meta.url);
 //const __dirname = path.dirname(__filename);
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    open:
-      mode === "simple" ? "/index-simplest-way-to-start.html" : "/index.html",
-  },
+export default {
   build: {
-    target: "es2019",
-    minify: "terser",
+    target: 'es2019',
+    minify: 'terser',
     lib: {
-      entry: __dirname + "/src/deepscatter.ts",
-      name: "Deepscatter",
-      formats: ["es"],
+      entry: __dirname + '/src/deepscatter.ts',
+      name: 'Deepscatter',
+      formats: ['es'],
     },
   },
 
   plugins: [
-    glslify({ compress: false }), // for debugging
+    glslify({ compress: false }), // for debugging 
     /*    worker({
       targetPlatform: 'browser',
       pattern: /(.+)\?worker.js/,
       //      extensions: supportedExts,
       preserveSource: true, // somehow results in slightly smaller bundle
     }), */
-  ],
-}));
+  ], 
+};
