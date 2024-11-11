cb409c1049b0c860fcb7b0262886c1ec2ac93f55
Ben Schmidt <bmschmidt@gmail.com>
2024-11-11 00:27:15 -0500

    refactor manifests to be lazier

--- dev/Everyone.svelte
+++ dev/Everyone.svelte
@@ -1,25 +1,20 @@
 <script>
-  import { Scatterplot } from '../src/deepscatter';
+  import { Deeptable, Scatterplot } from '../src/deepscatter';
   import { onMount } from 'svelte';
   import SwitchPositions from './svelte/SwitchPositions.svelte';
   import ColorChange from './svelte/ColorChange.svelte';
   import SizeSlider from './svelte/SizeSlider.svelte';
   import PositionScales from './svelte/PositionScales.svelte';
   import SelectPoints from './svelte/SelectPoints.svelte';
-  const startSize = 2;
+  const startSize = 0.8;
   const prefs = {
-    source_url: '/newtiles',
-    max_points: 10,
+    max_points: 100000,
     alpha: 35, // Target saturation for the full page.
-    zoom_balance: 0.22, // Rate at which points increase size. https://observablehq.com/@bmschmidt/zoom-strategies-for-huge-scatterplots-with-three-js
+    zoom_balance: 0.05, // Rate at which points increase size. https://observablehq.com/@bmschmidt/zoom-strategies-for-huge-scatterplots-with-three-js
     point_size: startSize, // Default point size before application of size scaling
     background_color: '#EEEDDE',
     duration: 100,
     encoding: {
-      color: {
-        field: 'class',
-        range: 'category10',
-      },
       x: {
         field: 'x',
         transform: 'literal',
@@ -32,19 +27,23 @@
   };
 
   let scatterplot = null;
-  onMount(() => {
-    scatterplot = new Scatterplot('#deepscatter');
+  onMount(async () => {
+    const tb = await Deeptable.fromQuadfeather({
+      baseUrl: 'http://localhost:8080/everyone',
+    });
+    window.tb = tb;
+    scatterplot = new Scatterplot({ selector: '#deepscatter', deeptable: tb });
     window.scatterplot = scatterplot;
     scatterplot.plotAPI(prefs);
   });
 </script>
 
 <div id="overlay">
-  <SwitchPositions {scatterplot}></SwitchPositions>
+  <!-- <SwitchPositions {scatterplot}></SwitchPositions>
   <ColorChange {scatterplot}></ColorChange>
   <SizeSlider size={startSize} {scatterplot}></SizeSlider>
   <PositionScales {scatterplot} />
-  <SelectPoints {scatterplot}></SelectPoints>
+  <SelectPoints {scatterplot}></SelectPoints> -->
 </div>
 
 <div id="deepscatter"></div>
